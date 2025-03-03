1a596cf957ef26ce6adc85aa179f1a5337e1a19b
“Iscan” <dev.haluk@gmail.com>
2025-03-04 01:52:44 +0300

    refactor start scripts in package.json for consistency; enhance error handling and logging in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
-// main.ts dosyanızda aşağıdaki değişiklikleri yapın
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  try {
+    const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT || 3000;
+
+    // CORS ayarlarını ekleyin
+    app.enableCors();
+
+    await app.listen(port, '0.0.0.0');
+    Logger.log(`Application is running on port: ${port}`);
+  } catch (error) {
+    Logger.error(`Failed to start application: ${error.message}`, error.stack);
+    process.exit(1);
+  }
 }
 bootstrap();
